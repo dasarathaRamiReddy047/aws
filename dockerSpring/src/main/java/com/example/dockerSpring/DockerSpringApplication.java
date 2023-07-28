633ff8df307978f3b2e6fab186ef5eda2967f884
dasarathaRamiReddy047 <132756705+dasarathaRamiReddy047@users.noreply.github.com>
2023-07-28 20:08:57 +0530

    Add files via upload

---
+++ dockerSpring/src/main/java/com/example/dockerSpring/DockerSpringApplication.java
@@ -0,0 +1,19 @@
+package com.example.dockerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
